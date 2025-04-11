91a6c81a01d39fa5f23730eb038cfae15d0a9498
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-04-11 23:23:39 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+ 
+ import java.util.ArrayList;
+ public class Main{
+     public static void main(String args[]){
+         ArrayList<Integer> list = new  ArrayList<Integer>();
+         list.add(5);
+         list.add(4);
+         list.add(8);
+         list.add(10);
+         
+         System.out.println(list);
+     }
+     }
+ 
+ 
+ import java.util.ArrayList;
+ public class Main{
+     public static void main(String args[]){
+         ArrayList<String> list = new ArrayList<String>();
+         list.add("Piyush");
+         list.add("kumar");
+         list.add("Singh");
+         list.add("manish");
+         
+         System.out.println(list);
+     }
+ }
+
